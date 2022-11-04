910ab3aba62483b2fd86cedf2bddd00d2d46396c
sayeed <sayeed.705@gmail.com>
2022-11-04 21:40:50 +0530

    added some sample data

---
+++ src/utils/Data/SampleManhwaData.ts
@@ -0,0 +1,127 @@
+export const sampleManhwaData = [
+  {
+    id: "01",
+    title: "Solo Leveling",
+    cover_image:
+      "https://mangatx.com/wp-content/uploads/2019/10/c28c05c7e232803ac538689a4dc0785a0b5a51der1-921-1244v2_hq-1-193x278.jpg",
+    alternate_title: [" I alone level up"],
+    rank: "01",
+    rating_details: {
+      general_rating: {
+        rating: "4.8",
+        votes: "13000",
+      },
+      our_rating: {
+        rating: "4.5",
+        votes: "1000",
+      },
+    },
+    description:
+      "Solo Leveling story : 10 years ago, after “the Gate” that connected the real world with the monster world opened, some of the ordinary, everyday people received the power to hunt monsters within the Gate. They are known as “Hunters”. However, not all Hunters are powerful. My name is Sung Jin-Woo, an E-rank Hunter. I’m someone who has to risk his life in the lowliest of dungeons, the “World’s Weakest”. Having no skills whatsoever to display, I barely earned the required money by fighting in low-leveled dungeons… at least until I found a hidden dungeon with the hardest difficulty within the D-rank dungeons! In the end, as I was accepting death, I suddenly received a strange power, a quest log that only I could see, a secret to leveling up that only I know about! If I trained in accordance with my quests and hunted monsters, my level would rise. Changing from the weakest Hunter to the strongest S-rank Hunter!",
+    genre: [
+      "Action",
+      "Adventure",
+      "Based on a Novel",
+      "Fantasy",
+      "Manhwa",
+      "Supernatural",
+    ],
+    status: "ENDED",
+    author: "N/A",
+    type: "Manhwa",
+    chapters: [
+      {
+        chapter_id: "sl_01",
+        chapter_release_date: "01/01/2020",
+        chapter_name: "Chapter 01",
+      },
+      {
+        chapter_id: "sl_02",
+        chapter_release_date: "02/01/2020",
+        chapter_name: "Chapter 02",
+      },
+      {
+        chapter_id: "sl_03",
+        chapter_release_date: "03/01/2020",
+        chapter_name: "Chapter 03",
+      },
+      {
+        chapter_id: "sl_04",
+        chapter_release_date: "04/01/2020",
+        chapter_name: "Chapter 04",
+      },
+      {
+        chapter_id: "sl_05",
+        chapter_release_date: "05/01/2020",
+        chapter_name: "Chapter 05",
+      },
+    ],
+  },
+  {
+    id: "02",
+    title: "The Beginning After the End",
+    cover_image:
+      "https://mangatx.com/wp-content/uploads/2019/11/the-b-193x278.jpg",
+    alternate_title: [
+      "The Beginning After the End",
+      "The Beginning of the End",
+    ],
+    rank: "02",
+    rating_details: {
+      general_rating: {
+        rating: "4.7",
+        votes: "12000",
+      },
+      our_rating: {
+        rating: "4.6",
+        votes: "1500",
+      },
+    },
+    description:
+      "Solo Leveling story : 10 years ago, after “the Gate” that connected the real world with the monster world opened, some of the ordinary, everyday people received the power to hunt monsters within the Gate. They are known as “Hunters”. However, not all Hunters are powerful. My name is Sung Jin-Woo, an E-rank Hunter. I’m someone who has to risk his life in the lowliest of dungeons, the “World’s Weakest”. Having no skills whatsoever to display, I barely earned the required money by fighting in low-leveled dungeons… at least until I found a hidden dungeon with the hardest difficulty within the D-rank dungeons! In the end, as I was accepting death, I suddenly received a strange power, a quest log that only I could see, a secret to leveling up that only I know about! If I trained in accordance with my quests and hunted monsters, my level would rise. Changing from the weakest Hunter to the strongest S-rank Hunter!",
+
+    genre: [
+      "Action",
+      "Adventure",
+      "Comedy, Comic",
+      "Drama",
+      "Fantasy",
+      "isekai",
+      "Reincarnation",
+      "Romance",
+      "Shounen",
+      "Supernatural",
+      "Webtoon",
+    ],
+    status: "ONGOING",
+    author: ["Fuyuki23", "TurtleMe"],
+    type: " Webtoon",
+    chapters: [
+      {
+        chapter_id: "tbad_01",
+        chapter_release_date: "01/01/2020",
+        chapter_name: "Chapter 01",
+      },
+      {
+        chapter_id: "tbad_02",
+        chapter_release_date: "02/01/2020",
+        chapter_name: "Chapter 02",
+      },
+      {
+        chapter_id: "tbad_03",
+        chapter_release_date: "03/01/2020",
+        chapter_name: "Chapter 03",
+      },
+      {
+        chapter_id: "tbad_04",
+        chapter_release_date: "04/01/2020",
+        chapter_name: "Chapter 04",
+      },
+      {
+        chapter_id: "tbad_05",
+        chapter_release_date: "05/01/2020",
+        chapter_name: "Chapter 05",
+      },
+    ],
+  },
+];
