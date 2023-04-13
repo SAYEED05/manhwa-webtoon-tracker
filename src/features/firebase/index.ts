cb66699f0c0a2205b0fec5d901885cccd7b19541
sayeedzh <sayeed.705@gmail.com>
2023-04-13 16:02:08 +0530

    removed analytics

--- src/features/firebase/index.ts
+++ src/features/firebase/index.ts
@@ -30,6 +30,6 @@ export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
+// export const analytics = getAnalytics(app);
 
 export const googleAuthProvider = new GoogleAuthProvider();
